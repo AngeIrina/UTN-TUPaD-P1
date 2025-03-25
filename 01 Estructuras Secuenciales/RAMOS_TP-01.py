471c790069725ac255a011cfd8b4873095020dd1
Angela <ramosangelairina@gmail.com>
2025-03-25 11:08:59 -0300

    TP-1 y actualización del archivo README.MD

---
+++ 01 Estructuras Secuenciales/RAMOS_TP-01.py
@@ -0,0 +1,71 @@
+# ESTRUCTURAS SECUENCIALES - TP 1. Angela Ramos.
+
+# 1.
+# print("Hola Mundo")
+
+
+# 2.
+#nombre_usuario = input("Ingrese su nombre: ")
+#print(f"Hola {nombre_usuario}, bienvenido/a.")
+
+
+# 3.
+#nombre = input("Ingrese su nombre: ")
+#apellido = input("Ingrese su apellido: ")
+#edad = int(input("Ingrese su edad: "))
+#lugar_residencia = input("Ingrese su lugar de residencia: ")
+#print(f"Hola. Soy {nombre} {apellido}, tengo {edad} años y vivo en {lugar_residencia}.")
+
+
+# 4.
+#radio = float(input("Ingrese el radio de un círculo: "))
+#area = 3.1416 * radio ** 2
+#perimetro = 2 * 3.1416 * radio
+#print(f"El área del círculo es {area} y el perímetro es {perimetro}.")
+
+
+# 5.
+#segundos = int(input("Ingrese una cantidad de segundos: "))
+#equivalente_horas = segundos // 3600
+#print(f"El equivalente en horas de {segundos} segundos es {equivalente_horas} horas.")
+
+
+# 6.
+#num = int(input("Escribe un número para ver su tabla de multiplicar: "))
+#for i in range(1, 11):
+#    print(f"{num} x {i} = {num * i}")
+
+
+# 7.
+#num1 = int(input("Escribe un número (distino de 0): "))
+#num2 = int(input("Escribe otro número (distino de 0): "))
+#suma = num1 + num2
+#resta = num1 - num2
+#multiplicacion = num1 * num2    
+#division = num1 / num2
+#print(f"La suma de {num1} y {num2} es {suma}.")
+#print(f"La resta de {num1} y {num2} es {resta}.")
+#print(f"La multiplicación de {num1} y {num2} es {multiplicacion}.")
+#print(f"La división de {num1} y {num2} es {division}.")
+
+
+# 8.
+#altura = float(input("Ingrese su altura en metros: "))
+#peso = float(input("Ingrese su peso en kilogramos: "))
+#imc = peso / altura ** 2
+#print(f"Su índice de masa corporal es {imc}.")
+
+
+# 9.
+#grados_celsius = float(input("Ingrese una temperatura en grados Celsius: "))
+#grados_fahrenheit = grados_celsius * 9 / 5 + 32
+#print(f"La temperatura en grados Fahrenheit es {grados_fahrenheit}.")
+
+
+# 10.
+#num1 = int(input("Ingresa el primer número: "))
+#num2 = int(input("Ingresa el segundo número: "))
+#num3 = int(input("Ingresa el tercer número: "))
+#promedio = (num1 + num2 + num3) / 3
+#print(f"El promedio de los números {num1}, {num2} y {num3} es {promedio}.")
+
